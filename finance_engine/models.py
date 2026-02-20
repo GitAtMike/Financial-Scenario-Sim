68e6f98e49e290a124988e9d5064c10d70cff458
Michael Huber <128461719+GitAtMike@users.noreply.github.com>
2026-02-19 23:05:31 -0600

    feat: added functions to models

---
+++ finance_engine/models.py
@@ -0,0 +1,24 @@
+class FinancialState:
+
+    def __init__(self, monthlyIncome, fixedExpense, varExpense, savingsBal, debtBal):
+        self.monthlyIncome = monthlyIncome
+        self.fixedExpense = fixedExpense
+        self.varExpense = varExpense
+        self.savingsBal = savingsBal
+        self.debtBal = debtBal
+
+
+    def getTotalFixed(self):
+        return sum(self.fixedExpense.values())
+
+    def getTotalVar(self):
+        return sum(self.varExpense.values())
+
+    def getTotalMonthlyExpense(self):
+        return self.getTotalFixed() + self.getTotalVar()
+
+    def getTotalMonthlyIncome(self):
+        return self.monthlyIncome
+
+    def getMonthlySurplus(self):
+        return self.getTotalMonthlyIncome() - self.getTotalMonthlyExpense()
